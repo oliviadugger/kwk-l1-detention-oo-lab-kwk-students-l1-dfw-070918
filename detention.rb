5c47b5fa1a71cead32f1784d81b5152b43b29073
oliviadugger <oliviacdugger@gmail.com>
2018-07-15 18:42:30 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,7 +1,6 @@
 #Code your detention class here
 class Detention
-  def test_location
-    @test_location = "Room 301"
+  def initialize(test.location)
+    @test.location = "Room 301"
 end
 
-Detention
